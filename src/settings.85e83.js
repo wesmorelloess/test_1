1f634c51ba0b031b709f3c439c4e6673fda61c71
wesmorelloess <wesmorelloess@gmail.com>
2024-08-14 19:20:09 +0800

    add v1

---
+++ src/settings.85e83.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/Loading.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d17e2",GameScene:"33973",main:"cf83f"}};
